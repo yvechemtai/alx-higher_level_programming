e1649bcdad2c55a5eb6126835e322d548a8b24ef
yvechemtai <yvechemtai@gmail.com>
2023-08-16 16:56:02 -0700

    more data structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-def find_best_score(scores_dict):
+def best_score(a_dictionary):
     """Returns a key with the biggest integer value."""
-    if not isinstance(scores_dict, dict) or len(scores_dict) == 0:
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
 
-    best_key = list(scores_dict.keys())[0]
-    highest_score = scores_dict[best_key]
-    for key, value in scores_dict.items():
+    best_key = list(a_dictionary.keys())[0]
+    highest_score = a_dictionary[best_key]
+    for key, value in a_dictionary.items():
         if value > highest_score:
             highest_score = value
             best_key = key
